6faef80e4d5827a835efe49b32173cf912bd333f
alejandroileal <aleal1202@outlook.com>
2023-12-04 18:52:00 +0100

    Add users model

---
+++ src/repo/users/users.mongo.model.ts
@@ -0,0 +1,46 @@
+import { Schema, model } from 'mongoose';
+import { User } from '../../entities/user';
+
+const usersSchema = new Schema<User>({
+  name: {
+    type: String,
+    required: true,
+  },
+  firstSurname: {
+    type: String,
+    required: true,
+  },
+  secondSurname: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+  },
+  profilePic: {
+    publicId: String,
+    size: Number,
+    height: Number,
+    width: Number,
+    format: String,
+    url: String,
+  },
+});
+
+usersSchema.set('toJSON', {
+  transform(_document, returnedObject) {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwd;
+  },
+});
+
+export const UserModel = model<User>('User', usersSchema, 'users');
