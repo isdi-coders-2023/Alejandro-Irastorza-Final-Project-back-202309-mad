bba67e293cb94705b9f6827d168f29b234fd915b
alejandroileal <aleal1202@outlook.com>
2023-12-04 17:13:19 +0100

    Add index and app to start server

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import cors from 'cors';
+import express from 'express';
+import morgan from 'morgan';
+
+export const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.static('public'));
