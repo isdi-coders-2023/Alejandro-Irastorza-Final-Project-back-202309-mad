da4f8754a0f6991d5e1449b69a1eadce23c90a72
alejandroileal <aleal1202@outlook.com>
2023-12-04 19:09:26 +0100

    Cors temporary remove

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,9 @@
-import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 
 export const app = express();
 app.disable('x-powered-by');
 
-app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static('public'));
