0e4bba67ba04420e7a9b16a13f4a76064ab89da1
alejandroileal <aleal1202@outlook.com>
2023-12-05 13:20:14 +0100

    Fix app folding

--- src/app.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import morgan from 'morgan';
-
-export const app = express();
-app.disable('x-powered-by');
-
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.static('public'));
