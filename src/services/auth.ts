aa828616800baed420f6ed27a36463a37de8ab6d
alejandroileal <aleal1202@outlook.com>
2023-12-08 13:52:16 +0100

    Add jwt to generate token

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,8 +1,16 @@
 import createDebug from 'debug';
 import { hash, compare } from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { User } from '../entities/user';
+import 'dotenv/config';
 
 const debug = createDebug('AB:auth');
 debug('Imported');
+
+export type TokenPayload = {
+  id: User['id'];
+  email: User['email'];
+} & jwt.JwtPayload;
 
 export abstract class Auth {
   static secret = process.env.JWT_SECRET;
@@ -14,5 +22,9 @@ export abstract class Auth {
 
   static compare(value: string, hashedPswd: string): Promise<boolean> {
     return compare(value, hashedPswd);
+  }
+
+  static getTokenJWT(payload: TokenPayload) {
+    return jwt.sign(payload, Auth.secret!);
   }
 }
