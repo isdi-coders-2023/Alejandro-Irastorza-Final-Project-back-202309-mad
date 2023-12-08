1d596521a81400820e412e4e1c52f9249ca10f01
alejandroileal <aleal1202@outlook.com>
2023-12-08 16:55:07 +0100

    Add media file tested

--- src/services/media.file.test.ts
+++ src/services/media.file.test.ts
@@ -1,19 +1,37 @@
 import { UploadApiResponse, v2 as cloudinary } from 'cloudinary';
 import { MediaFiles } from './media.file';
 import { ImgData } from '../types/img.data';
+import { HttpError } from '../types/http.error/http.error';
 
 jest.mock('cloudinary');
 
-test('then uploadImageToCloudinary should ', async () => {
-  (cloudinary.uploader.upload as jest.Mock).mockResolvedValue(
-    {} as UploadApiResponse
-  );
+const mediaFiles = new MediaFiles();
 
-  const mediaFiles = new MediaFiles();
+describe('Given Auth class', () => {
+  describe('when uploadImageToCloudinary is succesful', () => {
+    test('then it should return an objet of type imageData ', async () => {
+      (cloudinary.uploader.upload as jest.Mock).mockResolvedValue(
+        {} as UploadApiResponse
+      );
+      const result = await mediaFiles.uploadImageToCloudinary('');
 
-  const result = await mediaFiles.uploadImageToCloudinary('');
+      expect(cloudinary.uploader.upload).toHaveBeenCalled();
+      expect(result).toEqual({} as ImgData);
+    });
 
-  expect(cloudinary.uploader.upload).toHaveBeenCalled();
+    describe('when uploadImageToCloudinary is not succesful', () => {
+      test('then it should throw an error ', async () => {
+        (cloudinary.uploader.upload as jest.Mock).mockRejectedValue(
+          Error('Error test')
+        );
 
-  expect(result).toEqual({} as ImgData);
+        try {
+          await mediaFiles.uploadImageToCloudinary('');
+        } catch (error) {
+          expect(error).toBeInstanceOf(TypeError);
+          expect(HttpError).toThrow();
+        }
+      });
+    });
+  });
 });
