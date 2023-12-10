f2c36dd8e3b51461edbfbe000ccda5c633d62ace
alejandroileal <aleal1202@outlook.com>
2023-12-10 14:28:54 +0100

    Add products router

---
+++ src/routers/products.router.ts
@@ -0,0 +1,21 @@
+import createDebug from 'debug';
+import { Router as createRouter } from 'express';
+import { ProductsMongoRepo } from '../repo/products/products.mongo.repo.js';
+import { ProdcutsController } from '../controllers/products.controller.js';
+import { FileInterceptor } from '../middleware/file.interceptor.js';
+
+const debug = createDebug('AB:products:router');
+
+export const productsRouter = createRouter();
+debug('Starting Products Router');
+
+const repo = new ProductsMongoRepo();
+const controller = new ProdcutsController(repo);
+const fileInterceptor = new FileInterceptor();
+
+productsRouter.get('/', controller.getAll.bind(controller));
+productsRouter.post(
+  '/admin/products',
+  fileInterceptor.singleFileStore('modelImg').bind(fileInterceptor),
+  controller.create.bind(controller)
+);
