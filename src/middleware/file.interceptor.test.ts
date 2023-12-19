58186469b7f1ebdc62d76ff09fddf53fb59dc4b6
alejandroileal <aleal1202@outlook.com>
2023-12-19 13:49:11 +0100

    Fix routes to match admin path

--- src/middleware/file.interceptor.test.ts
+++ src/middleware/file.interceptor.test.ts
@@ -1,4 +1,5 @@
 import { FileInterceptor } from './file.interceptor';
+import 'crypto';
 
 jest.mock('crypto', () => ({
   randomUUID: jest.fn().mockReturnValue(''),
@@ -11,4 +12,27 @@ describe('Given FileInterceptorClass', () => {
     const result = fileInterceptor.singleFileStore();
     expect(typeof result).toBe('function');
   });
+
+  // Test('it should set up multer with expected options', () => {
+  //   const multerInstance = multer({
+  //     storage: multer.diskStorage({
+  //       destination: './public/uploads',
+  //       filename(_req, file, callback) {
+  //         const prefix = crypto.randomUUID();
+  //         callback(null, prefix + '-alien-bar-' + file.originalname);
+  //       },
+  //     }),
+  //     limits: { fileSize: 8000000 },
+  //   });
+
+  //   const middleware = fileInterceptor.singleFileStore();
+
+  //   const mockRequest = {} as Request;
+  //   const mockResponse = {} as Response;
+  //   const mockNext = jest.fn() as NextFunction;
+
+  //   middleware(mockRequest, mockResponse, mockNext);
+
+  //   expect(multerInstance).toBeDefined();
+  // });
 });
--- src/routers/products.router.ts
+++ src/routers/products.router.ts
@@ -3,6 +3,7 @@ import { Router as createRouter } from 'express';
 import { ProductsMongoRepo } from '../repo/products/products.mongo.repo.js';
 import { ProdcutsController } from '../controllers/products.controller.js';
 import { FileInterceptor } from '../middleware/file.interceptor.js';
+import { AuthInterceptor } from '../middleware/auth.interceptor.js';
 
 const debug = createDebug('AB:products:router');
 
@@ -12,20 +13,32 @@ debug('Starting Products Router');
 const repo = new ProductsMongoRepo();
 const controller = new ProdcutsController(repo);
 const fileInterceptor = new FileInterceptor();
+const interceptor = new AuthInterceptor();
 
 productsRouter.get('/', controller.getAll.bind(controller));
 productsRouter.post(
-  '/admin/add',
+  '/admin/add/:userId',
+  interceptor.authorization.bind(interceptor),
   fileInterceptor.singleFileStore('modelImg').bind(fileInterceptor),
   controller.create.bind(controller)
 );
 
 productsRouter.patch(
   '/admin/update/:id',
+  interceptor.authorization.bind(interceptor),
   fileInterceptor.singleFileStore('modelImg').bind(fileInterceptor),
   controller.update.bind(controller)
 );
 
-productsRouter.delete('/admin/delete/:id', controller.delete.bind(controller));
+productsRouter.delete(
+  '/admin/delete/:id',
+  interceptor.authorization.bind(interceptor),
+  controller.delete.bind(controller)
+);
 
 productsRouter.get('/admin/find/:id', controller.getById.bind(controller));
+
+productsRouter.get(
+  '/admin/category/:category',
+  controller.getByCategory.bind(controller)
+);
