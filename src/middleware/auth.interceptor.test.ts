8b1aa29c10196b27d1a9bf82a18f2f6817f574df
alejandroileal <aleal1202@outlook.com>
2023-12-19 17:51:19 +0100

    Update test

--- src/middleware/auth.interceptor.test.ts
+++ src/middleware/auth.interceptor.test.ts
@@ -1,20 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
 import { AuthInterceptor } from './auth.interceptor';
 import { HttpError } from '../types/http.error/http.error';
-import { TokenPayload } from '../services/auth';
+import { Auth } from '../services/auth';
 
-jest.mock('../services/auth', () => ({
-  verifyAndGetPayload: jest
-    .fn()
-    .mockReturnValue({ email: '123', id: '123' } as TokenPayload),
-}));
+jest.mock('../services/auth');
 
 const interceptor = new AuthInterceptor();
 
 const mockRequest = {
   get: jest.fn(() => 'Bearer 12345'),
   body: {
-    userId: '' as TokenPayload['id'],
+    userId: '',
   },
 } as unknown as Request;
 
@@ -26,14 +22,24 @@ const mockResponse = {} as Response;
 
 const mockNext = jest.fn() as NextFunction;
 
-test('should ', () => {
-  interceptor.authorization(mockRequest, mockResponse, mockNext);
-  expect(mockNext).toHaveBeenCalled();
-  expect(mockRequest.body.userId).toStrictEqual('');
-});
+describe('Given AuthInterceptor class', () => {
+  describe('When request has no Authorization header ', () => {
+    test('then it should throw an error', () => {
+      interceptor.authorization(
+        mockRequestWithNoHeader,
+        mockResponse,
+        mockNext
+      );
 
-test('should ', () => {
-  interceptor.authorization(mockRequestWithNoHeader, mockResponse, mockNext);
+      expect(mockNext).toHaveBeenCalledWith(new HttpError(404, 'Unautorized'));
+    });
+  });
 
-  expect(mockNext).toHaveBeenCalledWith(new HttpError(404, 'Unautorized'));
+  describe('When request has valid Authorization header', () => {
+    test('then it should add userId to the request body ', () => {
+      (Auth.verifyAndGetPayload as jest.Mock).mockReturnValue({ id: '123' });
+      interceptor.authorization(mockRequest, mockResponse, mockNext);
+      expect(mockRequest.body.userId).toBe('123');
+    });
+  });
 });
