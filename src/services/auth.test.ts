2dcbf1950e8f42d23a89a7b65e4db095217d49f4
alejandroileal <aleal1202@outlook.com>
2023-12-19 21:17:29 +0100

    Update test describes

--- src/services/auth.test.ts
+++ src/services/auth.test.ts
@@ -7,42 +7,51 @@ jest.mock('jsonwebtoken');
 
 describe('Given Auth abstract class', () => {
   describe('When we use its methods', () => {
-    test('Then hashPassword should... ', () => {
-      (hash as jest.Mock).mockReturnValue('Test value');
-      const result = Auth.hashPassword('');
-      expect(hash).toHaveBeenCalled();
-      expect(result).toBe('Test value');
+    describe('When we use hashPassword method', () => {
+      test('then it should return hashed password', () => {
+        (hash as jest.Mock).mockReturnValue('Test value');
+        const result = Auth.hashPassword('');
+        expect(hash).toHaveBeenCalled();
+        expect(result).toBe('Test value');
+      });
     });
 
-    test('Then compare should ', () => {
-      (compare as jest.Mock).mockReturnValue(true);
-      const result = Auth.compare('', '');
-      expect(compare).toHaveBeenCalled();
-      expect(result).toBe(true);
+    describe('When we use compare method', () => {
+      test('then it should return true for matching passwords', () => {
+        (compare as jest.Mock).mockReturnValue(true);
+        const result = Auth.compare('', '');
+        expect(compare).toHaveBeenCalled();
+        expect(result).toBe(true);
+      });
     });
 
-    test('Then getTokenJWT should ', () => {
-      (jwt.sign as jest.Mock).mockReturnValue(true);
-      const result = Auth.getTokenJWT({} as TokenPayload);
-      expect(jwt.sign).toHaveBeenCalled();
-      expect(result).toBe(true);
+    describe('When we use getTokenJWT method', () => {
+      test('then it should return a token', () => {
+        (jwt.sign as jest.Mock).mockReturnValue(true);
+        const result = Auth.getTokenJWT({} as TokenPayload);
+        expect(jwt.sign).toHaveBeenCalled();
+        expect(result).toBe(true);
+      });
     });
 
-    test('Then verifyAndGetPayload should ', () => {
-      (jwt.verify as jest.Mock).mockReturnValue(true);
-      const result = Auth.verifyAndGetPayload('');
-      expect(jwt.verify).toHaveBeenCalled();
-      expect(result).toBe(true);
+    describe('When we use verifyAndGetPayload method', () => {
+      test('then it should verify token and return payload', () => {
+        (jwt.verify as jest.Mock).mockReturnValue(true);
+        const result = Auth.verifyAndGetPayload('');
+        expect(jwt.verify).toHaveBeenCalled();
+        expect(result).toBe(true);
+      });
     });
   });
 
   describe('When its methods fail', () => {
-    test('then verifyAndGetPayload should throw an error', () => {
+    test('then verifyAndGetPayload method should throw an error', () => {
       (jwt.verify as jest.Mock).mockReturnValue('');
 
       try {
         Auth.verifyAndGetPayload('');
       } catch (error) {
+        // Define the expected error message or assertion as needed
         expect((error as Error).message).toBe('');
       }
     });
